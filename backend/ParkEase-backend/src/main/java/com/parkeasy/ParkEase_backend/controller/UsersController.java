42575dcdbf4a807c6a35be31ab1c2d4c8a697331
Atharvital23 <atharvital08@gmail.com>
2026-02-19 22:16:16 +0530

    Resolved merge conflict in UsersController

--- backend/ParkEase-backend/src/main/java/com/parkeasy/ParkEase_backend/controller/UsersController.java
+++ backend/ParkEase-backend/src/main/java/com/parkeasy/ParkEase_backend/controller/UsersController.java
@@ -1,9 +1,15 @@
 package com.parkeasy.ParkEase_backend.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.parkeasy.ParkEase_backend.dto.UsersRequestDTO;
 import com.parkeasy.ParkEase_backend.entity.Users;
@@ -26,6 +32,13 @@ public class UsersController {
 	}
 
 	// 2️⃣ Register User
+
+	@GetMapping("/{user_id}")
+	public ResponseEntity<Optional<Users>> getUserById(@PathVariable int user_id) {
+		Optional<Users> users = usersService.getAllUsers(user_id);
+		return ResponseEntity.ok(users);
+	}
+
 	@PostMapping
 	public ResponseEntity<Users> createUser(@RequestBody UsersRequestDTO usersRequestDTO) {
 		return ResponseEntity.ok(usersService.createUsers(usersRequestDTO));
